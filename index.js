bbc6fff02e6582e87431345157cc520feec2aa55
onyangoouma <jacksonouma04@gmail.com>
2022-06-16 19:41:59 +0300

    completed

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Add your code here
+function submitData(name, email){
+    return fetch("http://localhost:3000/users", {
+        method: "POST",
+        headers: {
+            "content-type": "application/json",
+            Accept: "application/json"
+        },
+        body: JSON.stringify({
+            name,
+            email
+
+        }),
+    })
+    .then(function(response){
+        return response.json();
+    })
+    .then(function(object){
+        console.log(object);
+        document.body.textContent = object["id"];
+    })
+    .catch(function(error){
+        document.body.textContent = error.message;
+    });
+}
